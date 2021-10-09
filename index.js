56e0afe072aec0c56481a767859c55082de37c43
Yeison <enmanuelquiterios@gmail.com>
2021-10-08 22:29:19 -0400

    commit tree 59f0c3551fa528a83d59063001af6952f1a1689d

--- index.js
+++ index.js
@@ -1,1 +1,76 @@
-// Your code here
+
+class Polygon {
+    constructor(count){
+        this.count = count;
+    }
+
+    get countSides (){
+
+        return this.count.length;
+
+
+    }
+
+    get perimeter(){
+        let counter = 0;
+        for(let i of this.count){
+            counter+= i;
+        }
+
+        return counter;
+
+
+
+        
+
+        
+
+    }
+
+
+}
+
+
+
+
+class Triangle extends Polygon {
+
+    get isValid () {
+        if(this.count[0] + this.count[1] > this.count[2]){
+            return true;
+        } return false
+
+    }
+
+ 
+
+ 
+
+}
+
+class Square extends Polygon{
+
+    constructor(base,height){
+        super(base,height)
+        this.base= base;
+        this.height = height;
+    }
+    get isValid (){
+
+        for(let i of this.data){
+            if(i === i){
+                return true
+            } return false;
+        }
+
+    }
+
+
+    get area(){
+        return (this.base * this.height) / 2
+
+    }
+
+}
+
+
